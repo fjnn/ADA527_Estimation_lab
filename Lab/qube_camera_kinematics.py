a3580dc9c6c56bcf0f0f9ee483f474d9aa5c252c
Gizem <gizem.ates@hvl.no>
2024-03-13 22:03:18 +0100

    that was stupid

--- Lab/qube_camera_kinematics.py
+++ Lab/qube_camera_kinematics.py
@@ -23,13 +23,13 @@ alpha = d2r(0)
 # T_motor_rodCOM = Link(ET.Rz(0) * ET.tx(l4) * ET.ty(l5*np.sin(alpha)) * ET.tz(l3-l5*np.cos(alpha)))
 # robot = rtb.Robot([T_w_motor, T_motor_rodCOM], name="qube robot")
 
-joint1 = rtb.RevoluteDH(d=0, a=0, alpha=0, offset=0, qlim=(-0.01, 0.01))
-joint2 = rtb.RevoluteDH(d=l3, alpha=np.pi/2, a=0, offset=0, qlim=(-0.01, 0.01))
-joint3 = rtb.RevoluteDH(d=l4, alpha=np.pi/2, a=0, offset=0, qlim=(-0.01, 0.01))
+# joint1 = rtb.RevoluteDH(d=0, a=0, alpha=0, offset=0, qlim=(-0.01, 0.01))
+joint1 = rtb.RevoluteDH(d=l3, alpha=np.pi/2, a=0, offset=0, qlim=(-0.01, 0.01))
+joint2 = rtb.RevoluteDH(d=l4, alpha=np.pi/2, a=0, offset=0, qlim=(-0.01, 0.01))
 ee = rtb.RevoluteDH(d=l5, a=0, alpha=0, offset=0, qlim=(-0.01, 0.01))
-qube_pendulum_robot = rtb.DHRobot([joint1, joint2, joint3, ee], name="Qube+pendulum")
+qube_pendulum_robot = rtb.DHRobot([joint1, joint2, ee], name="Qube+pendulum")
 
-q = np.array([0, theta, alpha, 0])
+q = np.array([theta, alpha, 0])
 print(qube_pendulum_robot.fkine(q))
 
 # qube_pendulum_robot.teach(q)
