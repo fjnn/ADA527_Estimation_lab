72ad0abc1eead902b3757c9dfbd0c061acdde4f4
fjnn <gizem.ates@hvl.no>
2025-04-02 10:41:37 +0200

    Create installation_test.py

---
+++ Lab/installation_test.py
@@ -0,0 +1,14 @@
+import cv2
+import numpy as np
+import yaml
+import spatialmath.base as base
+from Classes import Qube
+from time import sleep
+from math import degrees
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+print("Installation done.")
